7a9682144b8ec5ce24a6547e4fa85642bc7e0355
AhmedAtef07 <ahmedatef07@hotmail.com>
2015-10-15 19:55:51 +0200

    Finished bisection algorithm

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-var properties = ["scale", "width", "height"];
+var properties = ['scale', 'width', 'height'];
 var scale = 40;
 var width = 600;
 var height = 300;
+var a = -5;
+var b = 5;
+var tolerance = 4;
+
+var constants = {
+  rangeLine: {
+    current: {
+      color: "rgb(66,44,255)",
+      thick: 2
+    },
+    old: {
+      color: "rgb(164, 154, 255)",
+      thick: 2
+    }
+  }
+};
+
+f = {};
+
+var algoStarted = false;
+
+var rangeLines = {};
 
 function runOnce() {
   var canvas = document.getElementById("canvas");
-  canvas.width = window.innerWidth - 350;
-  canvas.height= window.innerHeight - 100;
+  canvas.width = window.innerWidth - 400;
+  canvas.height= window.innerHeight - 70;
   width = canvas.width;
   height = canvas.height;
+
+
+  $('#a, #b, #tolerance').bind("propertychange change click keyup input paste", function(event) {
+    checkEquation(event);
+  });  
+  $('#a').val(a);
+  $('#b').val(b);
+  $('#tolerance').val(tolerance);
+  addPropertiesToSideBar();
+
   tryToDraw();
 }
 
@@ -22,12 +54,21 @@ function draw(fun) {
   var axes = {};
   axes.x0 = width / 2;
   axes.y0 = height / 2;
+  axes.color = "rgb(128,128,128)";
+  axes.thick = 2;
 
   showAxes(ctx, axes);
 
-  // showRangeLine(ctx, axes, 100, "rgb(66,44,255)", 2);
-  // showRangeLine(ctx, axes, -100, "rgb(66,44,255)", 2);
-  if(fun !== undefined) funcGraph(ctx, axes, fun, "rgb(11,153,11)", 2);
+
+  if(fun !== undefined) f = fun;
+  if(f !== undefined) {
+    funcGraph(ctx, axes, f, "rgb(11,153,11)", 2);
+  }
+
+
+  for(var i in rangeLines) {
+    showRangeLine(ctx, axes, rangeLines[i], i);    
+  }
 
 }
 
@@ -37,14 +78,16 @@ function showAxes(ctx, axes) {
   var y0 = axes.y0, h = height;
 
   ctx.beginPath();
-  ctx.strokeStyle = "rgb(128,128,128)"; 
+  ctx.strokeStyle = axes.color;
+  ctx.lineWidth = axes.thick;
   ctx.moveTo(0, y0);  ctx.lineTo(w,y0);  // X axis
   ctx.moveTo(x0, 0);  ctx.lineTo(x0,h);  // Y axis
 
   var iMax = Math.round(width - x0);
   var iMin = Math.round(-x0);
 
-  ctx.font = "14px Georgia";
+  ctx.font = "14px Arial";
+  ctx.fillStyle = axes.color;
   ctx.textBaseline = "middle";
 
   // Drawing the y-axis numbers
@@ -97,16 +140,41 @@ function funcGraph (ctx,axes,func,color,thick) {
  ctx.stroke();
 }
 
-function showRangeLine(ctx, axes, x, color, thick) {
+function showRangeLine(ctx, axes, rangeLine, step) {
   var x0 = axes.x0;
   var y0 = axes.y0;
+  var x = rangeLine.x * scale;
+  var letter = rangeLine.letter;
+  var color = rangeLine.color;
 
   ctx.beginPath();
-  ctx.lineWidth = thick;
+  ctx.lineWidth = rangeLine.thick;
   ctx.strokeStyle = color;
 
-  ctx.moveTo(x0 + x, 0);
-  ctx.lineTo(x0 + x, y0 * 2);
+  drawLine(ctx, x0 + x, 13, x0 + x, y0 * 2 - 13);
+
+  ctx.font = "800 14px Arial";
+  ctx.fillStyle = color;
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText(letter, x0 + x, 5);
+  if(step != undefined) ctx.fillText(step, x0 + x, y0 * 2 - 5);
 
   ctx.stroke();
+}
+
+function addNewRangeLine(letter, x) {
+  var rangeLine = {
+    letter: letter,
+    x: x, 
+    color: constants.rangeLine.current.color,
+    thick: constants.rangeLine.current.thick 
+  };
+  for(var i in rangeLines) {
+    if (rangeLines[i].letter == letter) {
+      rangeLines[i].color = constants.rangeLine.old.color;
+      rangeLines[i].thick = constants.rangeLine.old.thick;
+    }
+  }
+  rangeLines.push(rangeLine);
 }
